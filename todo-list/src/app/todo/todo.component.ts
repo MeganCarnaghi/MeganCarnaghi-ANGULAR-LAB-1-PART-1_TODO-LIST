26a879ac66d0746f3c8f050d8078ce3d578b5dab
Megan <carnagh5@gmail.com>
2021-03-30 22:05:46 -0400

    added structure to todo component and styling

--- todo-list/src/app/todo/todo.component.ts
+++ todo-list/src/app/todo/todo.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo/todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
+  todos: Todo[] = [
+    {
+      task: 'do laundry',
+      completed: false,
+    },
+    {
+      task: 'walk dog',
+      completed: true,
+    },
+    {
+      task: 'finish homework',
+      completed: false,
+    },
+    {
+      task: 'meal prep',
+      completed: false,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
